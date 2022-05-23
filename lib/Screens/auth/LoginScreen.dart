3ab3c0427f6bc5facdd59997fc03c713ff815912
Jonathan Rojas Granda <jonargp1@gmail.com>
2022-05-23 23:12:50 +0200

    Merge LoginScreen biometrics

--- lib/Screens/auth/LoginScreen.dart
+++ lib/Screens/auth/LoginScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:swapjobapp/Screens/skillListScreen.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '/Utils/color.dart';
 import '../Home.dart';
 import '../../../Utils/requests.dart';
@@ -10,12 +11,29 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginScreen> {
+  late LocalAuthentication _localAth;
+
+  bool _isBiometricAvailable = false;
+  bool _userWantsBiometrics = true;
+
   bool _isLoading = false;
-  String email = 'pako@astapor.com';
-  String password = 'P@ssw0rd';
+  String email = '';
+  String password = '';
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _localAth = LocalAuthentication();
+    _localAth.canCheckBiometrics.then((result) {
+      setState(() {
+        _isBiometricAvailable = result;
+        checkFingerPrint();
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -105,67 +123,68 @@ class _LoginState extends State<LoginScreen> {
               ),
               email.isNotEmpty && password.isNotEmpty
                   ? Padding(
-                      padding: const EdgeInsets.only(bottom: 40),
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: InkWell(
-                          child: Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.rectangle,
-                                  borderRadius: BorderRadius.circular(25),
-                                  gradient: LinearGradient(
-                                      begin: Alignment.topRight,
-                                      end: Alignment.bottomLeft,
-                                      colors: [
-                                        thirdBlueColor.withOpacity(.5),
-                                        thirdBlueColor.withOpacity(.8),
-                                        thirdBlueColor,
-                                        thirdBlueColor
-                                      ])),
-                              height: MediaQuery.of(context).size.height * .065,
-                              width: MediaQuery.of(context).size.width * .75,
-                              child: Center(
-                                  child: Text(
-                                "CONTINUE",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    color: textColor,
-                                    fontWeight: FontWeight.bold),
-                              ))),
-                          onTap: () {
-                            setState(() {
-                              _isLoading = true;
-                            });
-                            login(
-                                emailController.text, passwordController.text);
-                          },
-                        ),
-                      ),
-                    )
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(25),
+                            gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  thirdBlueColor.withOpacity(.5),
+                                  thirdBlueColor.withOpacity(.8),
+                                  thirdBlueColor,
+                                  thirdBlueColor
+                                ])),
+                        height: MediaQuery.of(context).size.height * .065,
+                        width: MediaQuery.of(context).size.width * .75,
+                        child: Center(
+                            child: Text(
+                              "CONTINUE",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: textColor,
+                                  fontWeight: FontWeight.bold),
+                            ))),
+                    onTap: () {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      if (_isBiometricAvailable && _userWantsBiometrics) {
+                        showAlertDialog(context);
+                      }
+                    },
+                  ),
+                ),
+              )
                   : Padding(
-                      padding: const EdgeInsets.only(bottom: 40),
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: InkWell(
-                          child: Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.rectangle,
-                                  borderRadius: BorderRadius.circular(25),
-                                  color: primaryOrangeColor),
-                              height: MediaQuery.of(context).size.height * .065,
-                              width: MediaQuery.of(context).size.width * .75,
-                              child: Center(
-                                  child: Text(
-                                "CONTINUE",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    color: textColor,
-                                    fontWeight: FontWeight.bold),
-                              ))),
-                          onTap: () {},
-                        ),
-                      ),
-                    )
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(25),
+                            color: primaryOrangeColor),
+                        height: MediaQuery.of(context).size.height * .065,
+                        width: MediaQuery.of(context).size.width * .75,
+                        child: Center(
+                            child: Text(
+                              "CONTINUE",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: textColor,
+                                  fontWeight: FontWeight.bold),
+                            ))),
+                    onTap: () {},
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -173,14 +192,13 @@ class _LoginState extends State<LoginScreen> {
     );
   }
 
-
   login(String email, String password) async {
-    bool success = await performLogin("pako@astapor.com", "P@ssw0rd");
+    bool success = await performLogin(email, "P@ssw0rd");
     //bool success = await performLogin("pako@astapor.com", "P@ssw0rd");
     if (success) {
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (BuildContext context) => HomePage()),
-          (Route<dynamic> route) => false);
+              (Route<dynamic> route) => false);
     } else {
       setState(() {
         _isLoading = false;
@@ -188,5 +206,67 @@ class _LoginState extends State<LoginScreen> {
             content: Text("The user not exists, bad credentials")));
       });
     }
+  }
+
+  void checkFingerPrint() {
+    SharedPreferences.getInstance().then((prefs) async {
+      _userWantsBiometrics = prefs.getBool("userWantsBiometrics") ?? true;
+      if (_userWantsBiometrics) {
+        if (prefs.getString('email') != null &&
+            prefs.getString('password') != null &&
+            prefs.getString('email') != '' &&
+            prefs.getString('password') != '') {
+          bool didAuthenticate = await _localAth.authenticate(
+              localizedReason: "Iniciar sesion con huella");
+          if (didAuthenticate) {
+            login(prefs.getString('email')!, prefs.getString('password')!);
+          }
+        }
+      }
+    });
+  }
+
+  void showAlertDialog(BuildContext context) {
+    // set up the buttons
+    Widget cancelButton = FlatButton(
+      child: Text("NO"),
+      onPressed: () {
+        SharedPreferences.getInstance().then((prefs) {
+          prefs.setBool("userWantsBiometrics", false);
+        });
+        _userWantsBiometrics = false;
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("YES"),
+      onPressed: () {
+        SharedPreferences.getInstance().then((prefs) {
+          prefs.setBool("userWantsBiometrics", true);
+          prefs.setString('email', emailController.text);
+          prefs.setString('password', passwordController.text);
+        });
+        _userWantsBiometrics = true;
+        Navigator.of(context).pop();
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Biometrics detected"),
+      content: Text("Would you like to use the biomethrics sensor?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    ).then((value) => login(emailController.text, passwordController.text));
   }
 }
